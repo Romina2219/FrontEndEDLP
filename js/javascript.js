929b60cee121cf199d880598a1c3d1e86fef9e44
Romina2219 <104035349+Romina2219@users.noreply.github.com>
2022-06-01 11:37:46 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+function inclusive(){
+  document.body.style.backgroundColor = "black";
+  document.getElementById("nuevo").style.backgroundColor = "black";
+  document.getElementById("nuevo").style.color = "white";
+  document.getElementById("subtitulo").style.color = "white";
+  document.getElementById("bajada").style.color = "white";
+  document.getElementById("texto").style.color = "white";
+  document.getElementById("texto2").style.color = "white";
+  document.getElementById("texto3").style.color = "white";
+  document.getElementById("ingredientes").style.color = "white";
+  document.getElementById("receta").style.color = "white";
+  document.getElementById("nuevo1").style.backgroundColor = "black";
+}
+function cartel() {alert("Romina Romero 2022")}
